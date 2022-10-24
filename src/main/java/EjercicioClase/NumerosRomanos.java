3bb5504f10095e4b82b96a02d31a174bcbbe7d06
Eduardo <eduardocorreaaranda10@gmail.com>
2022-10-24 19:32:56 +0200

    ejercicio final

--- src/main/java/EjercicioClase/NumerosRomanos.java
+++ src/main/java/EjercicioClase/NumerosRomanos.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
-
 package EjercicioClase;
 
 import javax.swing.JOptionPane;
@@ -14,13 +13,14 @@ import javax.swing.JOptionPane;
 public class NumerosRomanos {
 
     public static void main(String[] args) {
-        
+
         //Programa que analice los números romanos y las letra que tiene tu 
         //nombre
-        
-        
-        int menu = Integer.parseInt(JOptionPane.showInputDialog
-        (null, """
+          int menu;
+
+          do{
+
+            menu = Integer.parseInt(JOptionPane.showInputDialog(null, """
                     --------------------------------------
                             MENU EJERCICIO
                           1º Números romanos 
@@ -29,28 +29,59 @@ public class NumerosRomanos {
                     --------------------------------------
                
                """));
-    
-       while(menu != 3){ 
-           
-           if(menu == 1){
-               
-           int numRomano = Integer.parseInt(JOptionPane.showInputDialog(
-                   null,"Introduce un número decimal "));
-           String numero1 = "I", numero2 = "II", numero3 = "III", 
-                  numero4 = "IV", numero5 = "V", numero6 = "VI",
-                  numero7 = "VII",numero8 = "VIII", numero9 = "IX",
-                  numero10 = "X"; 
-           if (numRomano == 1){
-               System.out.println(numero1);
 
-           }else{
-               System.out.println(numero2);
-                    
-           }
-           
-       }    
-            
-             
-       }   
+            switch (menu) {
+                case 1:
+                    int numRomano = Integer.parseInt(JOptionPane.showInputDialog(
+                            null, "Introduce un número decimal del 1 al 10: "));
+
+                    switch (numRomano) {
+                        case 1 ->
+                            JOptionPane.showMessageDialog(null, "I");
+                        case 2 ->
+                            JOptionPane.showMessageDialog(null, "II");
+                        case 3 ->
+                            JOptionPane.showMessageDialog(null, "III");
+                        case 4 ->
+                            JOptionPane.showMessageDialog(null, "IV");
+                        case 5 ->
+                            JOptionPane.showMessageDialog(null, "V");
+                        case 6 ->
+                            JOptionPane.showMessageDialog(null, "VI");
+                        case 7 ->
+                            JOptionPane.showMessageDialog(null, "VII");
+                        case 8 ->
+                            JOptionPane.showMessageDialog(null, "VIII");
+                        case 9 ->
+                            JOptionPane.showMessageDialog(null, "IX");
+                        case 10 ->
+                            JOptionPane.showMessageDialog(null, "X");
+                    }
+                    break;
+
+                case 2:
+
+                    String nombre = JOptionPane.showInputDialog("Inserte un nombre: ").toLowerCase();
+                    char letra;
+                    int contador = 0;
+                    for (int i = 0; i < nombre.length(); i++) {
+                       letra = nombre.charAt(i); 
+                       if (letra == 'a'|| letra == 'e'|| letra == 'i' || letra == 'o'|| letra == 'u' ){  
+                         contador = contador + 1; 
+                       }
+                       
+                    }
+
+                    JOptionPane.showMessageDialog(null, "El nombre tiene: " + contador + " vocales");
+
+                    break;
+
+
+                default:
+                    JOptionPane.showMessageDialog(null, "Número equivocado ");
+
+            }
+
+        }while(menu !=3);
     }
 }
